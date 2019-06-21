0d9afdfcfe88f6e992969d67d2bc1fb140b7c8d0
dcafonso <danielcarlosafonso@gmail.com>
2019-06-21 11:56:07 -0300

    Listas e templates veiculos, mensalistas, e movtos

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,22 @@
 from django.urls import include, re_path
-from .views import home, lista_pessoas
+from .views import (
+    home,
+    lista_pessoas,
+    lista_veiculos,
+    lista_mov_rotativos,
+    lista_mensalistas,
+    lista_mov_mensalistas
+)
+
 
 urlpatterns = [
     re_path(r'^$', home, name='core_home'),
-    re_path(r'^pessoas/$', lista_pessoas, name='core_lista_pessoas')
+    re_path(r'^pessoas/$', lista_pessoas, name='core_lista_pessoas'),
+    re_path(r'^veiculos/$', lista_veiculos, name='core_lista_veiculos'),
+    re_path(r'^mov_rotativos/$', lista_mov_rotativos,
+            name='core_lista_mov_rotativos'),
+    re_path(r'^mensalistas/$', lista_mensalistas,
+            name='core_lista_mensalistas'),
+    re_path(r'^mov_mensalistas', lista_mov_mensalistas,
+            name='core_lista_mov_mensalistas')
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from .models import Pessoa
+from .models import (
+    Pessoa,
+    Veiculo,
+    MovtoRotativo,
+    Mensalista,
+    MovtoMensalista
+)
 
 
 def home(request):
@@ -10,3 +16,23 @@ def home(request):
 def lista_pessoas(request):
     pessoas = Pessoa.objects.all()
     return render(request, 'core/lista_pessoas.html', {'pessoas': pessoas})
+
+
+def lista_veiculos(request):
+    veiculos = Veiculo.objects.all()
+    return render(request, 'core/lista_veiculos.html', {'veiculos': veiculos})
+
+
+def lista_mov_rotativos(request):
+    mov_rotativos = MovtoRotativo.objects.all()
+    return render(request, 'core/lista_mov_rotativos.html', {'mov_rotativos': mov_rotativos})
+
+
+def lista_mensalistas(request):
+    mensalistas = Mensalista.objects.all()
+    return render(request, 'core/lista_mensalistas.html', {'mensalistas': mensalistas})
+
+
+def lista_mov_mensalistas(request):
+    mov_mensalistas = MovtoMensalista.objects.all()
+    return render(request, 'core/lista_mov_mensalistas.html', {'mov_mensalistas': mov_mensalistas})
