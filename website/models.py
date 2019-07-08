33a7336f6fa1ad2fa220652d20751e70271e670c
dcafonso <danielcarlosafonso@gmail.com>
2019-07-08 13:08:41 -0300

    implementação do model para salvar o contato

--- website/models.py
+++ website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Contato(models.Model):
+    email = models.EmailField()
+    nome = models.CharField(max_length=60)
+    sobrenome = models.CharField(max_length=60)
+    mensagem = models.TextField()
+
+    def __str__(self):
+        return self.nome
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Contato
 
 
 def home(request):
@@ -7,7 +8,22 @@ def home(request):
 
 
 def contato(request):
-    return render(request, 'website/contato.html')
+    mensagem = ''
+
+    if request.method == 'POST':
+        try:
+            contato = {}
+            contato['email'] = request.POST.get('email')
+            contato['nome'] = request.POST.get('nome')
+            contato['sobrenome'] = request.POST.get('sobrenome')
+            contato['mensagem'] = request.POST.get('mensagem')
+            Contato.objects.create(**contato)
+        except Exception as e:
+            mensagem = str(e)
+        else:
+            mensagem = 'Mensagem cadastrada com sucesso.'
+
+    return render(request, 'website/contato.html', {'mensagem': mensagem})
 
 
 def servicos(request):
